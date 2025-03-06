0e56b4cc394f03faffb838b3372395c2c7c4d638
Kenneth Leung <kennethleung.ty@gmail.com>
2025-03-06 15:33:58 +0800

    Modify base notebook and add data, with inclusion of simple-evals submodule

---
+++ samplers/ollama_sampler.py
@@ -0,0 +1,16 @@
+import ollama
+
+
+class OllamaSampler:
+    def __init__(self, model_name=None):
+        self.model_name = model_name
+
+    def __call__(self, prompt_messages):
+        prompt_text = prompt_messages[-1]["content"]
+        response = ollama.chat(
+            model=self.model_name, messages=[{"role": "user", "content": prompt_text}]
+        )
+        return response["message"]["content"]
+
+    def _pack_message(self, content, role):
+        return {"role": role, "content": content}
